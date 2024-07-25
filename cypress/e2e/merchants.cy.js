3c116410d57fc399be612ff2353d7db326773013
Jerome Encinares <rmncnrs16@gmail.com>
2024-07-25 22:10:05 +0800

    update testing on merchant

--- cypress/e2e/merchants.cy.js
+++ cypress/e2e/merchants.cy.js
@@ -1,17 +1,17 @@
 describe('7connect Module: Merchants', () => {
-  it('should navigate to "Search Merchant" under the merchants module', () => {
-    cy.auth()
-    cy.merchant();
-    cy.get('.jdm_events > :nth-child(1) > a').click().wait(3000);
-    cy.logout();
-  })
+  // it('should navigate to "Search Merchant" under the merchants module', () => {
+  //   cy.auth()
+  //   cy.merchant();
+  //   cy.get('.jdm_events > :nth-child(1) > a').click().wait(3000);
+  //   cy.logout();
+  // })
   
-  it('should navigate to "New Merchant" under the merchants module', () => {
-    cy.auth()
-    cy.merchant();
-    cy.get('.jdm_events > :nth-child(2) > a').click().wait(3000);
-    cy.logout();
-  })
+  // it('should navigate to "New Merchant" under the merchants module', () => {
+  //   cy.auth()
+  //   cy.merchant();
+  //   cy.get('.jdm_events > :nth-child(2) > a').click().wait(3000);
+  //   cy.logout();
+  // })
 
   it('should navigate to "Search Merchant" and display the expected output in the first row of the table', () => {
     cy.auth()
@@ -26,21 +26,41 @@ describe('7connect Module: Merchants', () => {
       .should('exist')
       .should('be.visible').click();
 
-      const firstResult = [
-        'Ensogo',
-        'ensogo',
-        'ecpay',
-        'ensogoMux', '2,880','True','False','False','True'
-      ];
-      
-      cy.get('.list > table > tbody > :nth-child(1)').within(() => {
-        cy.get('td').each((cell, i) => {
-          cy.wrap(cell)
-          .should('exist')
-          .should('be.visible')
-          .should('have.text', firstResult[i]);
+      // const result = [
+      //   'Ensogo',
+      //   'ensogo',
+      //   'ecpay',
+      //   'ensogoMux', '2,880','True','False','False','True'
+      // ];
+
+      // cy.get('tbody > :nth-child(1) > :nth-child(3)')
+      // cy.get('.list > table > tbody > :nth-child(1)').within(() => {
+      //   cy.get('td').each((cell, i) => {
+      //     cy.wrap(cell)
+      //     .should('exist')
+      //     .should('be.visible')
+      //     .should('have.text', result[i]);
+      //   });
+      // });
+  
+      // cy.get('.paginateButtons > :nth-child(2)').click()
+
+      let result = [];
+
+      for (let index = 10; index <= 10; index++) {
+        cy.get(`.list > table > tbody > :nth-child(${index})`).within(() => {
+          cy.get('td').each((cell, i) => {
+            result.push(cell.text())
+            cy.wrap(cell)
+            .should('exist')
+            .should('be.visible')
+            .should('have.text', result[i]).wait(200);
+            // cy.log(cell.text())
+          });
+          result = [];
         });
-      });
+      }
+
   })
   })
 
